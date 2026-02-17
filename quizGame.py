2dc23a6b2bb93f51d81f7a7712ff2b59702c4eb1
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-02-17 08:19:36 +0000

    quizGame

---
+++ quizGame.py
@@ -0,0 +1,12 @@
+print("Welcome to my Quiz")
+play=input("Do you want to play(Y/N)? ")
+
+if play=='N':
+    quit()
+if play!='Y' and play!='N':
+    print("Error")
+    quit()
+    print(1)
+
+print("Let's Play!")
+
