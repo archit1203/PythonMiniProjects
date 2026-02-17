66b36546bf89de47ba3068a98c214810db219f03
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-02-17 11:40:32 +0000

    numGuesser

--- numGuesser.py
+++ numGuesser.py
@@ -1,12 +1,18 @@
 import random
 num=random.randint(0,100) 
-print(num)
+#print(num)
 guess=1
 count=0
 print("Guess the number between 1 and 100!")
 while(guess):
+    try:
+        user = int(input("Enter your guess: "))
+    except ValueError:
+        print("Please enter a valid number!")
+        continue
+
     count+=1
-    user=int(input("Enter your guess: "))
+
     if user==num:
         print("\nGuessed Correctly!!!")
         print(f'You took {count} tries')
